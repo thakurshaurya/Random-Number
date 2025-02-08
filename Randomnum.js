890cdfcda2fc02e1a0ae5f7b615faf37fc3b92e1
thakurshaurya <thakurshaurya2007@gmail.com>
2025-02-08 15:10:42 +0530

    New Changes

--- Randomnum.js
+++ Randomnum.js
@@ -1,15 +1,28 @@
 let Out;
 let guess;
+let count = 0;
 Out = Math.floor(Math.random() * 10 + 1)
 document.getElementById("btn").onclick = function () {
     guess = Number(document.getElementById("inp").value);
     if (Out == guess) {
         document.getElementById("Congrat").textContent = "!!! Congratulation !!!";
-        document.getElementById("Out").textContent = `The Number Was ${Out}`;
+        document.getElementById("Out").textContent = `The Number Was ${Out}, You Tried ${count} Times`;
         document.getElementById("image").style.display = "block"
     }
     else {
         document.getElementById("Congrat").textContent = "Try Again :(";
+        count++;
+        document.getElementById("inp").value = "";
+
     }
 }
-document.getElementById("inp").value = 1;
+document.getElementById("inp").value = "";
+document.getElementById("btnreset").onclick = function () {
+    document.getElementById("inp").value = "";
+    document.getElementById("Congrat").textContent = "";
+    document.getElementById("Out").textContent = ``;
+    document.getElementById("image").style.display = "";
+    count = 0;
+    Out = Math.floor(Math.random() * 10 + 1)
+
+}
